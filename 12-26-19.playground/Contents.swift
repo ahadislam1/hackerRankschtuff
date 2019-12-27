6ebed04b4f04eb95f244247bf90212dc0cd33b3e
Ahad Islam <ahad.islam624@gmail.com>
2019-12-26 23:12:44 -0500

    did some HackerRank recursive insertion function. its really bad.

---
+++ 12-26-19.playground/Contents.swift
@@ -0,0 +1,46 @@
+
+import Foundation
+
+class Node {
+    let data: Int
+    var next: Node?
+
+    init(data: Int) {
+        self.data = data
+    }
+}
+
+
+func insert(head: Node?, data: Int!) -> Node? {
+    
+    guard let head = head else {
+        return Node(data: data)
+    }
+    
+    if head.next == nil {
+        head.next = Node(data: data)
+    } else {
+        _ = insert(head: head.next, data: data)
+    }
+    
+    return head
+}
+
+func display(head: Node?) {
+    var current = head
+
+    while current != nil {
+        print(current!.data, terminator: " ")
+        current = current!.next
+    }
+}
+
+var head: Node?
+var arr = [2, 3, 4, 1]
+
+for num in arr {
+    print("Current number is :\(num)")
+    head = insert(head: head, data: num)
+}
+
+display(head: head)
